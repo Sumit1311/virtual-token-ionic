890a1d45902322dde52e4caec6ec6ad10295c623
Sumit Toshniwal <sumit.toshniwal@ibm.com>
2020-07-24 16:11:12 +0530

    Added new account id

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,5 +2,5 @@ export default {
     API_HOST: "https://virtualtoken-dev.herokuapp.com",
     //ACCOUNT_ID: "522a1f20-cc9f-11ea-bd80-c7887d7ae273",
     //API_HOST: "http://localhost:3000",
-    ACCOUNT_ID: "3bb17260-cca0-11ea-80cf-5ba100aef335"
+    ACCOUNT_ID: "f533fed0-cd98-11ea-833b-f3cf5bc234f8"
 }
