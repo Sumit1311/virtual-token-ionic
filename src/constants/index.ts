0caabc5e39c7c0c0a7cf7caffe9cc5934fc7bd9e
Sumit Toshniwal <sumit.toshniwal@ibm.com>
2020-07-25 07:59:30 +0530

    Added updated api url

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,5 @@
 export default {
-    API_HOST: "https://virtualtoken-dev.herokuapp.com",
+    API_HOST: "https://virtualtoken-dev-api.herokuapp.com",
     //ACCOUNT_ID: "522a1f20-cc9f-11ea-bd80-c7887d7ae273",
     //API_HOST: "http://localhost:3000",
     ACCOUNT_ID: "f533fed0-cd98-11ea-833b-f3cf5bc234f8"
