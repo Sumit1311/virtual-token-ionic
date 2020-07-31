e4ae1b42f50e3b5c77cb38c99aa4ffa28948d08b
Sumit Toshniwal <sumit.toshniwal@ibm.com>
2020-07-31 09:35:36 +0530

    Add comma

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,7 @@
 export default {
     //API_HOST: "http://localhost:3000",
     DASHBOARD_URL: "/dashboard",
-    LOGIN_FAILURE: "Invalid username or password"
+    LOGIN_FAILURE: "Invalid username or password",
     API_HOST: "https://virtualtoken-dev-api.herokuapp.com",
     //ACCOUNT_ID: "f533fed0-cd98-11ea-833b-f3cf5bc234f8"
 }
